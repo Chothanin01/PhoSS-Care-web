1945b1731837d256f88613d9306b964b0b53748f
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2026-01-29 23:22:08 +0700

    Create account + dropdown

---
+++ component/Combobox.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/components/ui/button";
+import { UserRoundPlus } from "lucide-react";
+import { IBM_Plex_Sans_Thai } from "next/font/google";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export function DropdownMenuDemo() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className="
+          flex items-center justify-center
+          gap-5
+          w-40
+          bg-[#05548D] text-white
+          rounded-[9px]
+          cursor-pointer
+          shadow-[0_6px_16px_rgba(5,84,141,0.3)]
+          text-md font-semibold
+          hover:bg-[#05548D]
+  "
+        >
+          สร้างบัญชี
+          <UserRoundPlus className="h-10 w-10 " strokeWidth={3} />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-40" align="start">
+        <DropdownMenuGroup>
+          <DropdownMenuLabel className="font-medium">
+            โรคทั้งหมด
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>โรคเบาหวาน</DropdownMenuItem>
+          <DropdownMenuItem>โรคความดันโลหิตสูง</DropdownMenuItem>
+          <DropdownMenuItem>วัณโรค</DropdownMenuItem>
+          <DropdownMenuItem>วัคซีนเด็ก</DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
