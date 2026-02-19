598a7d5284bcb90352e458a24a1bc8905ef05959
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2026-02-20 02:26:20 +0700

    RPAT-007 ข้อมูลผูป่วย
    - เพิ่มข้อมูลที่ใช้ mock
    - ทำ component card ข้อมูลส่วนตัวผู้ป่วย
    - onClick ปุ่มเเก้ไขเเล้ว

---
+++ app/(private)/patient/_components/patient-viewdata.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { Patient, Gender } from "@/app/utils/patient.mock";
+import { Pencil } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  patient: Patient;
+};
+
+export default function PatientCard({ patient }: Props) {
+  const router = useRouter();
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-6">ข้อมูลผู้ป่วย</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-80">
+        <div>
+          <h2 className="text-lg font-semibold mb-4">ข้อมูลส่วนตัว</h2>
+
+          {/* Gender */}
+          <div className="flex items-center gap-6 mb-4 ">
+            <label className="flex items-center gap-2 =">
+              <input
+                type="radio"
+                className="accent-Bamboo-100"
+                checked={patient.gender === Gender.MALE}
+                readOnly
+              />
+              ผู้ชาย
+            </label>
+
+            <label className="flex items-center gap-2">
+              <input
+                type="radio"
+                className="accent-Bamboo-100"
+                checked={patient.gender === Gender.FEMALE}
+                readOnly
+              />
+              ผู้หญิง
+            </label>
+          </div>
+
+          <div className="space-y-2 text-sm">
+            <p>
+              ชื่อ - นามสกุล : {patient.firstName} {patient.lastName}
+            </p>
+
+            <p>
+              อายุ : {patient.age} ปี {patient.month} เดือน {patient.day} วัน
+            </p>
+
+            <p>HN : {patient.hnId}</p>
+            <p>เลขบัตรประชาชน : {patient.nationalId}</p>
+            <p>สิทธิการรักษา : {patient.right}</p>
+
+            <div className="flex gap-50">
+              <p>สัญชาติ : {patient.nationality}</p>
+              <p>เชื้อชาติ : {patient.ethnicity}</p>
+            </div>
+
+            <div className="flex gap-46">
+              <p>น้ำหนัก : {patient.weight} กก.</p>
+              <p>ส่วนสูง : {patient.height} ซม.</p>
+            </div>
+
+            <p>การแพ้ยา : {patient.allergy}</p>
+            <p>เบอร์โทรศัพท์ : {patient.phone}</p>
+          </div>
+        </div>
+        <div className="">
+          <h2 className="text-lg font-semibold mb-4">ที่อยู่</h2>
+
+          <div className="grid grid-cols-2 gap-y-2 text-sm gap-20">
+            <p>เลขที่ : {patient.address.houseNo}</p>
+            <p>หมู่ที่ : {patient.address.village}</p>
+            <p>ถนน : {patient.address.road}</p>
+            <p>อำเภอ : {patient.address.district}</p>
+            <p>ตำบล : {patient.address.subDistrict}</p>
+            <p>จังหวัด : {patient.address.province}</p>
+            <p>รหัสไปรษณีย์ : {patient.address.postalCode}</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Edit Button */}
+      <div className="flex justify-end mt-8">
+        <button
+          onClick={() => router.push(`/patient/${patient.id}/edit/patient`)}
+          className="flex items-center gap-2 px-4 py-2 text-Bamboo-100 bg-white border-2 border-Bamboo-100 font-semibold rounded-lg hover:bg-Bamboo-100 hover:text-white"
+        >
+          แก้ไขข้อมูล
+          <Pencil size={16} />
+        </button>
+      </div>
+    </div>
+  );
+}
