0a4cea1312e82894c2987df960754dd0eff609b7
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2026-02-21 23:52:31 +0700

    RPAT-013
    เพิ่มข้อมูล mockup

--- app/(private)/patient/[id]/view/disease/[diseaseId]/_components/diseases-view.tsx
+++ app/(private)/patient/[id]/view/disease/[diseaseId]/_components/diseases-view.tsx
@@ -9,7 +9,7 @@ export default function Page({ data }: DiseasesViewProps) {
     <div className="ml-70 px-6 py-28">
       <div className="w-full bg-white p-6 rounded-lg shadow">
         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-3 gap-4 sm:gap-0">
-          <h2 className="text-2xl font-bold">ประวัติการรักษาโรค</h2>
+          <h2 className="text-2xl font-bold">ประวัติโรค</h2>
         </div>
       </div>
     </div>
