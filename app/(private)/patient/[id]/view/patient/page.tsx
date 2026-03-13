b5253f5420d2aae073a37073c1324c852610d295
naphatchanun <naphatchanun.suechey@gmail.com>
2026-03-13 14:39:34 +0700

    RPAT-014
    - integrate API ดูข้อมูลผู้ป่วย

--- app/(private)/patient/[id]/view/patient/page.tsx
+++ app/(private)/patient/[id]/view/patient/page.tsx
@@ -1,37 +1,22 @@
 "use client";
 
-import { useParams } from "next/navigation";
 import PatientCard from "@/app/(private)/patient/_components/patient-viewdata";
 import RelativeSection from "@/app/(private)/patient/_components/relative-viewdata";
 import OfficerSection from "@/app/(private)/patient/_components/hospital-viewdata";
-import { mockPatients } from "@/app/utils/patient.mock";
 
 export default function Page() {
-  const params = useParams();
-  const id = Number(params.id);
-
-  const patient = mockPatients.find((p) => p.id === id);
-
-  if (!patient) {
-    return (
-      <div className="ml-70 px-6 py-28">
-        <div className="w-full bg-white p-6 rounded-lg shadow">
-          ไม่พบข้อมูลผู้ป่วย
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="ml-70 px-6 py-28 space-y-3">
-      <div className="w-full bg-white p-6 rounded-lg shadow ">
-        <PatientCard patient={patient} />
+      <div className="w-[1190px] bg-white p-6 rounded-lg shadow">
+        <PatientCard />
       </div>
+
       <div className="w-full bg-white p-6 rounded-lg shadow">
-        <RelativeSection patient={patient} />
+        <RelativeSection />
       </div>
+
       <div className="w-full bg-white p-6 rounded-lg shadow">
-        <OfficerSection patient={patient} />
+        <OfficerSection />
       </div>
     </div>
   );
--- app/(private)/patient/_components/patient-viewdata.tsx
+++ app/(private)/patient/_components/patient-viewdata.tsx
@@ -1,14 +1,79 @@
 "use client";
-import { Patient, Gender } from "@/app/utils/patient.mock";
+
+import { useEffect, useState } from "react";
 import { Pencil } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
+import Cookies from "js-cookie";
 
-type Props = {
-  patient: Patient;
-};
-
-export default function PatientCard({ patient }: Props) {
+export default function PatientCard() {
   const router = useRouter();
+  const params = useParams();
+  const patientId = params.id;
+  console.log("id:", patientId);
+
+  const [patient, setPatient] = useState<any>(null);
+
+  useEffect(() => {
+    if (!patientId) return
+
+    const fetchPatient = async () => {
+      try {
+        const token = Cookies.get("token")
+
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/v1/admins/patients/${patientId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+
+        const data = await res.json()
+
+        console.log("patient api:", data)
+
+        const p = data?.data?.[0]?.patient
+        if (!p) return
+
+        setPatient({
+          id: patientId,
+          gender: p.sex,
+          firstName: p.fullname.split(" ")[0],
+          lastName: p.fullname.split(" ").slice(1).join(" "),
+          age: p.age_years,
+          month: p.age_months,
+          day: p.age_days,
+          sex: p.sex,
+          hnId: p.hnnumber,
+          nationalId: p.idcard,
+          right: p.rights,
+          nationality: p.nationality,
+          ethnicity: p.ethnicity,
+          weight: p.weight,
+          height: p.height,
+          allergy: p.allergy,
+          phone: p.phone_number,
+          address: {
+            house_number: p.address.house_number,
+            village: p.address.village_number,
+            road: p.address.road,
+            district: p.address.district,
+            subDistrict: p.address.subdistrict,
+            province: p.address.province,
+            postalCode: p.address.zipcode,
+          },
+        })
+      } catch (err) {
+        console.error("fetch patient error", err)
+      }
+    }
+
+    fetchPatient()
+  }, [patientId])
+
+  if (!patient) return <div className="p-8">Loading...</div>;
+
   return (
     <div className="p-8">
       <h1 className="text-xl font-semibold mb-6">ข้อมูลผู้ป่วย</h1>
@@ -18,32 +83,17 @@ export default function PatientCard({ patient }: Props) {
           <h2 className="font-semibold mb-2">ข้อมูลส่วนตัว</h2>
 
           <div className="flex items-center gap-6 mb-4">
-            {patient.gender === Gender.MALE && (
-              <label className="flex items-center gap-2">
-                <input
-                  type="radio"
-                  className="accent-Bamboo-100"
-                  checked
-                  readOnly
-                />
-                ผู้ชาย
-              </label>
-            )}
-
-            {patient.gender === Gender.FEMALE && (
-              <label className="flex items-center gap-2">
-                <input
-                  type="radio"
-                  className="accent-Bamboo-100"
-                  checked
-                  readOnly
-                />
-                ผู้หญิง
-              </label>
-            )}
+            <label className="flex items-center gap-2">
+              <input
+                type="radio"
+                className="accent-Bamboo-100"
+                checked
+                readOnly
+              />
+              {patient.sex === "Male" ? "ผู้ชาย" : "ผู้หญิง"}
+            </label>
           </div>
-
-          <div className="space-y-2 text-sm">
+          <div className="space-y-3 text-sm">
             <p>
               ชื่อ - นามสกุล : {patient.firstName} {patient.lastName}
             </p>
@@ -63,17 +113,18 @@ export default function PatientCard({ patient }: Props) {
 
             <div className="flex gap-46">
               <p>น้ำหนัก : {patient.weight} กก.</p>
-              <p>ส่วนสูง : {patient.height} ซม.</p>
+              <p className="px-1">ส่วนสูง : {patient.height} ซม.</p>
             </div>
 
             <p>การแพ้ยา : {patient.allergy}</p>
             <p>เบอร์โทรศัพท์ : {patient.phone}</p>
           </div>
         </div>
-        <div className="">
-          <h2 className="text-lg font-semibold mb-4">ที่อยู่</h2>
 
-          <div className="grid grid-cols-2 gap-x-20 text-sm gap-y-2">
+        <div>
+          <h2 className="font-semibold mb-4">ที่อยู่</h2>
+
+          <div className="grid grid-cols-2 gap-x-20 text-sm space-y-3">
             <p>เลขที่ : {patient.address.house_number}</p>
             <p>หมู่ที่ : {patient.address.village}</p>
             <p>ถนน : {patient.address.road}</p>
